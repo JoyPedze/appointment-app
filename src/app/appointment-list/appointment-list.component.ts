17163bfcfecc0a121a117e29e205901d3d8fc9c2
Joy Pedze <joypedped@gmail.com>
2024-08-08 15:20:40 +0200

    Added ngOnInit to load appointments

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import { Appointment } from '../models/appointment';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
   styleUrl: './appointment-list.component.css'
 })
-export class AppointmentListComponent {
+export class AppointmentListComponent implements OnInit{
   appointmentTitle:string = "";
   appointmentDate:Date = new Date();
   appointments:Appointment[] = [];
+
+  ngOnInit(): void {
+    let savedAppointments = localStorage.getItem("appointments");
+    this.appointments = savedAppointments ? JSON.parse(savedAppointments) : [];
+  }
 
   addAppointment(){
     //check if fields are not empty
@@ -25,10 +31,13 @@ export class AppointmentListComponent {
       //resetting the fields
       this.appointmentTitle = "";
       this.appointmentDate = new Date();
+
+      localStorage.setItem("appointments", JSON.stringify(this.appointments));
     }
   }
 
   deleteAppointment(index: number){
     this.appointments.splice(index, 1);
+    localStorage.setItem("appointments", JSON.stringify(this.appointments));
   }
 }
